ade2f5a3c432221b69680baf5822342d7bf4c8d0
nmihayl <78086030+nmihayl@users.noreply.github.com>
2024-02-29 14:26:06 +0100

    Responsive table layouts and other small changes

--- members-nick.php
+++ members-nick.php
@@ -18,58 +18,102 @@ $results = $connection->query('SELECT * FROM Label WHERE Member = "Nick"');
 </header>
 <main>
     <h1>
-        <img src="favicon-32x32.png" alt="logo" class="logo">
+    <a href="members.html"><img src="media/placeholder.png" href="members.html" alt="logo" class="logo"></a>
         RELEASES
         <img src="media/baller.png" alt="Fixed Image" class="baller">
     </h1>
     <div class="release-table">
         <?php
-        echo '<table>';
-        echo '
-        <tr>
-        <td>Release</td>
-        <td>Date</td>
-        <td>Type</td>
-        <td>Catalog Number</td>
-        <td>Format</td>
-        <td>Streaming</td>
-        <td>RateYourMusic</td>
-        </tr>';
-        while ($row = $results->fetchArray(SQLITE3_ASSOC)) {
-            echo '<tr>';
+            //desktop table layout
+            echo '<table class="desktop-table">';
             echo '
-            <td><img src="media/releases/' . $row['Cat'] . '.jpg" width="200px"> <b> ' . $row['Alias'] . ' </b> <br> ' . $row['ReleaseName'] . ' </td>
-                <td>' . $row['ReleaseDate'] . '</td>
-                <td>' . $row['ReleaseType'] . '</td>
-                <td>' . $row['Cat'] . '</td>
-                <td>' . $row['ReleaseFormat'] . '</td>
-                <td class="streaming">';
-            
-                // Check if Bandcamp link exists
-                if (!empty($row['StreamBC'])) {
-                    echo '<a href="' . $row['StreamBC'] . '" target="_blank"><img class="streaming" src="media/bandcamp.png" width="50px"></a>';
-                }
-                // Check if Spotify link exists
-                if (!empty($row['StreamS'])) {
-                    echo '<a href="' . $row['StreamS'] . '" target="_blank"><img class="streaming" src="media/spotify.png" width="50px"></a>';
-                }
-                // Check if Apple Music link exists
-                if (!empty($row['StreamAM'])) {
-                    echo '<a href="' . $row['StreamAM'] . '" target="_blank"><img class="streaming" src="media/applemusic.png" width="50px"></a>';
-                }
-                echo '</td>
-                    <th><a href="' . $row['RYM'] . '" target="_blank"><img src="media/rym.png" width="50px"></a></th>
-                ';
-                echo '</tr>';
-            }
-            echo '</table>';
+            <tr>
+            <td>Release</td>
+            <td>Date</td>
+            <td>Type</td>
+            <td>Catalog Number</td>
+            <td>Format</td>
+            <td>Stream</td>
+            <td>Meta</td>
+            </tr>';
+            while ($row = $results->fetchArray(SQLITE3_ASSOC)) {
+                echo '<tr>';
+                echo '
+                    <td><img src="media/releases/' . $row['Cat'] . '.jpg" width="200px"><br><b> ' . $row['Alias'] . ' </b> <br> ' . $row['ReleaseName'] . ' </td>
+                    <td>' . $row['ReleaseDate'] . '</td>
+                    <td>' . $row['ReleaseType'] . '</td>
+                    <td>' . $row['Cat'] . '</td>
+                    <td>' . $row['ReleaseFormat'] . '</td>
+                    <td class="streaming">';
+                        // Check if Bandcamp link exists
+                        if (!empty($row['StreamBC'])) {
+                            echo '<a href="' . $row['StreamBC'] . '" target="_blank"><img class="streaming" src="media/bandcamp.png" width="50px"></a>';
+                        }
+                    
+                        // Check if Spotify link exists
+                        if (!empty($row['StreamS'])) {
+                            echo '<a href="' . $row['StreamS'] . '" target="_blank"><img class="streaming" src="media/spotify.png" width="50px"></a>';
+                        }
+                    
+                        // Check if Apple Music link exists
+                        if (!empty($row['StreamAM'])) {
+                            echo '<a href="' . $row['StreamAM'] . '" target="_blank"><img class="streaming" src="media/applemusic.png" width="50px"></a>';
+                        }
+
+                    
+                        echo '</td>
+                            <th><a href="' . $row['RYM'] . '" target="_blank"><img class="streaming" src="media/rym.png" width="50px"></a></th>
+                        ';
+                        echo '</tr>';
+                    }
+                        echo '</table>';
+
+
+            //mobile table layout
+            echo '<table class="mobile-table">';
+            echo '
+            <tr>
+            </tr>';
+            while ($row = $results->fetchArray(SQLITE3_ASSOC)) {
+                echo '<tr>';
+                echo '
+                    <td><img src="media/releases/' . $row['Cat'] . '.jpg" width="200px"> <b> ' . $row['Alias'] . ' </b> <br> ' . $row['ReleaseName'] . ' </td>
+                    <td>'
+                     . $row['ReleaseDate'] . '
+                     <br>'
+                      . $row['ReleaseType'] . '
+                    <br>'
+                    . $row['Cat'] . '
+                    <br>'
+                    . $row['ReleaseFormat'] . 
+                    '</td>
+                    <td class="streaming">';
+                        // Check if Bandcamp link exists
+                        if (!empty($row['StreamBC'])) {
+                            echo '<a href="' . $row['StreamBC'] . '" target="_blank"><img class="streaming" src="media/bandcamp.png" width="50px"></a>';
+                        }
+                    
+                        // Check if Spotify link exists
+                        if (!empty($row['StreamS'])) {
+                            echo '<a href="' . $row['StreamS'] . '" target="_blank"><img class="streaming" src="media/spotify.png" width="50px"></a>';
+                        }
+                    
+                        // Check if Apple Music link exists
+                        if (!empty($row['StreamAM'])) {
+                            echo '<a href="' . $row['StreamAM'] . '" target="_blank"><img class="streaming" src="media/applemusic.png" width="50px"></a>';
+                        }
+
+                        // Check if RateYourMusic link exists
+                        if (!empty($row['RYM'])) {
+                            echo '<a href="' . $row['RYM'] . '" target="_blank"><img class="streaming" src="media/rym.png" width="50px"></a>';
+                        }
+                        echo '</tr>';
+                    }
+                        echo '</table>';
             ?>
     </div>
 </main>
-    <footer>
-        <br>
-        <a class="sticky-button" href="members.html">Return</a>
-        <br>
-    </footer>
+<footer>
+</footer>
 </body>
 </html>
--- releases.php
+++ releases.php
@@ -11,20 +11,20 @@ $results = $connection->query('SELECT * FROM Label');
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <meta name=viewport content="width=device-width, initial-scale=1, user-scalable=yes">
-    <link rel="icon" type="image/png" sizes="16x16" href="favicon-32x32.png">
 </head>
 <body>
     <header>
     </header>
     <main>
         <h1>
-        <img src="favicon-32x32.png" alt="logo" class="logo">
+        <a href="index.html"><img src="media/placeholder.png" href="index.html" alt="logo" class="logo"></a>
             RELEASES
             <img src="media/baller.png" alt="Fixed Image" class="baller">
         </h1>
         <div class="release-table">
             <?php
-            echo '<table>';
+            //desktop table layout
+            echo '<table class="desktop-table">';
             echo '
             <tr>
             <td>Release</td>
@@ -32,13 +32,13 @@ $results = $connection->query('SELECT * FROM Label');
             <td>Type</td>
             <td>Catalog Number</td>
             <td>Format</td>
-            <td>Streaming</td>
-            <td>RateYourMusic</td>
+            <td>Stream</td>
+            <td>Meta</td>
             </tr>';
             while ($row = $results->fetchArray(SQLITE3_ASSOC)) {
                 echo '<tr>';
                 echo '
-                    <td><img src="media/releases/' . $row['Cat'] . '.jpg" width="200px"> <b> ' . $row['Alias'] . ' </b> <br> ' . $row['ReleaseName'] . ' </td>
+                    <td><img src="media/releases/' . $row['Cat'] . '.jpg" width="200px"><br><b> ' . $row['Alias'] . ' </b> <br> ' . $row['ReleaseName'] . ' </td>
                     <td>' . $row['ReleaseDate'] . '</td>
                     <td>' . $row['ReleaseType'] . '</td>
                     <td>' . $row['Cat'] . '</td>
@@ -58,6 +58,7 @@ $results = $connection->query('SELECT * FROM Label');
                         if (!empty($row['StreamAM'])) {
                             echo '<a href="' . $row['StreamAM'] . '" target="_blank"><img class="streaming" src="media/applemusic.png" width="50px"></a>';
                         }
+
                     
                         echo '</td>
                             <th><a href="' . $row['RYM'] . '" target="_blank"><img class="streaming" src="media/rym.png" width="50px"></a></th>
@@ -65,13 +66,53 @@ $results = $connection->query('SELECT * FROM Label');
                         echo '</tr>';
                     }
                         echo '</table>';
+
+
+            //mobile table layout
+            echo '<table class="mobile-table">';
+            echo '
+            <tr>
+            </tr>';
+            while ($row = $results->fetchArray(SQLITE3_ASSOC)) {
+                echo '<tr>';
+                echo '
+                    <td><img src="media/releases/' . $row['Cat'] . '.jpg" width="200px"> <b> ' . $row['Alias'] . ' </b> <br> ' . $row['ReleaseName'] . ' </td>
+                    <td>'
+                     . $row['ReleaseDate'] . '
+                     <br>'
+                      . $row['ReleaseType'] . '
+                    <br>'
+                    . $row['Cat'] . '
+                    <br>'
+                    . $row['ReleaseFormat'] . 
+                    '</td>
+                    <td class="streaming">';
+                        // Check if Bandcamp link exists
+                        if (!empty($row['StreamBC'])) {
+                            echo '<a href="' . $row['StreamBC'] . '" target="_blank"><img class="streaming" src="media/bandcamp.png" width="50px"></a>';
+                        }
+                    
+                        // Check if Spotify link exists
+                        if (!empty($row['StreamS'])) {
+                            echo '<a href="' . $row['StreamS'] . '" target="_blank"><img class="streaming" src="media/spotify.png" width="50px"></a>';
+                        }
+                    
+                        // Check if Apple Music link exists
+                        if (!empty($row['StreamAM'])) {
+                            echo '<a href="' . $row['StreamAM'] . '" target="_blank"><img class="streaming" src="media/applemusic.png" width="50px"></a>';
+                        }
+
+                        // Check if RateYourMusic link exists
+                        if (!empty($row['RYM'])) {
+                            echo '<a href="' . $row['RYM'] . '" target="_blank"><img class="streaming" src="media/rym.png" width="50px"></a>';
+                        }
+                        echo '</tr>';
+                    }
+                        echo '</table>';
                         ?>
         </div>
-    </main>
-    <footer>
-        <br>
-        <a class="sticky-button" href="index.html">Return</a>
-        <br>
-    </footer>
+</main>
+<footer>
+</footer>
 </body>
 </html>
